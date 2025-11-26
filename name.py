e0ce8669694e3480b92b916f3c37e417ab89c006
goliboy <mudholhound866@gmail.com>
2025-11-26 18:54:23 +0530

    Add script to print script name and student name

---
+++ name.py
@@ -0,0 +1,8 @@
+import sys
+if len(sys.argv)!=2:
+print("usage: python student.py <name>")
+sys.exit(1)
+script_name = sys.argv[0]
+name = sys.argv[1]
+print("script name:",script_name)
+print("student name:",name)
